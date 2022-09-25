cbc668ffe458ee02857e671d8c5cd2307df1bcfd
adrianvillagomez <adrian132892@gmail.com>
2022-09-25 15:56:03 -0300

    Agregando la vista

---
+++ Entidades/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public abstract class Usuario
+    {
+        protected string nombre;
+        protected string apellido;
+        protected string mail;
+        protected string contraseña;
+
+        protected string Nombre { get => nombre; set => nombre = value; }
+        protected string Mail { get => mail; set => mail = value; }
+        protected string Contraseña { get => contraseña; }
+        protected string Apellido { get => apellido; set => apellido = value; }
+
+        protected Usuario(string nombre, string mail, string contraseña)
+        {
+            this.nombre = nombre;
+            this.mail = mail;
+            this.contraseña = contraseña;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre : {this.Nombre}");
+            sb.AppendLine($"Mail : {this.Mail}");
+            
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Vista/FrmIngreso.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Vista
+{
+    partial class FrmIngreso
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMail = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(68, 137);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(30, 15);
+            this.lblMail.TabIndex = 0;
+            this.lblMail.Text = "Mail";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(143, 299);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 1;
+            this.btnIngresar.Text = "button1";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(68, 195);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(67, 15);
+            this.lblContraseña.TabIndex = 2;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // FrmIngreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.lblMail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmIngreso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
